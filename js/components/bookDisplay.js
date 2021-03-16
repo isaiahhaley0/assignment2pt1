1f7c7a60bdf68912655103ee253f13f29653e541
isaiahhaley0 <isaiah.haley@ucdenver.edu>
2021-03-15 19:38:14 -0600

    thumbnail + pagaition

--- js/components/bookDisplay.js
+++ js/components/bookDisplay.js
@@ -9,10 +9,13 @@ app.component('book-display', {
     template:
         /*html*/
         `<ul class="col-sm-12 col-md-6 col-xl-4">
+        
     <a class="list-group-item list-group-item-action active" :href="this.bookObj.selfLink">{{this.bookObj.volumeInfo.title}}</a>
     <li class="list-group-item">{{this.bookObj.volumeInfo.authors}}</li>
     <li class="list-group-item">{{this.bookObj.volumeInfo.publisher}}, {{this.bookObj.volumeInfo.publishedDate}}</li>
-    </ul>`,
+    <img v-if="this.bookObj.volumeInfo.imageLinks.thumbnail != undefined" :src="this.bookObj.volumeInfo.imageLinks.thumbnail" width="150" height="150">
+    </ul>
+     `,
     computed: {
         bookObj() {
             if (this.book != null)
--- js/main.js
+++ js/main.js
@@ -2,14 +2,38 @@ const app = Vue.createApp({
     data() {
         return {
             keyword: '',
+            curretPage: 0,
             result: null
         }
     },
+    "pagination": {
+        "total": 10000,
+        "per_page": 50,
+        "current_page": 15,
+        "last_page": 200,
+        "next_page_url": "/api/stories?page=16",
+        "prev_page_url": "/api/stories?page=14",
+        "from": 751,
+        "to": 800,
+      },
     methods: {
         searchGoogleBooks() {
-            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex=0&maxResults=20")
+             
+            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + "&startIndex="+ this.curretPage*20 + "&maxResults=20")
             .then(response => response.json())
             .then(json => this.result = json)       
+        },
+        nextPage() {
+            this.curretPage += 1
+            this.searchGoogleBooks()    
+        },
+        prevPage() {
+
+
+            if(this.curretPage > 0){
+            this.curretPage -= 1
+            this.searchGoogleBooks()
+            }    
         }
     }
 })
